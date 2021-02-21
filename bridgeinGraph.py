37ce1c27edd4c8ee7a69dedfe0e6b2d216efedd6
Nitindrdarker <oct2001nitin@gmail.com>
2021-02-22 02:04:22 +0530

    better understanding of finding bridges in graph

--- bridgeinGraph.py
+++ bridgeinGraph.py
@@ -1,96 +1,33 @@
-from collections import defaultdict 
-   
-#This class represents an undirected graph using adjacency list representation 
-class Graph: 
-   
-    def __init__(self,vertices): 
-        self.V= vertices #No. of vertices 
-        self.graph = defaultdict(list) # default dictionary to store graph 
-        self.Time = 0
-   
-    # function to add an edge to graph 
-    def addEdge(self,u,v): 
-        self.graph[u].append(v) 
-        self.graph[v].append(u) 
-   
-    '''A recursive function that finds and prints bridges 
-    using DFS traversal 
-    u --> The vertex to be visited next 
-    visited[] --> keeps tract of visited vertices 
-    disc[] --> Stores discovery times of visited vertices 
-    parent[] --> Stores parent vertices in DFS tree'''
-    def bridgeUtil(self,u, visited, parent, low, disc): 
-  
-        # Mark the current node as visited and print it 
-        visited[u]= True
-  
-        # Initialize discovery time and low value 
-        disc[u] = self.Time 
-        low[u] = self.Time 
-        self.Time += 1
-  
-        #Recur for all the vertices adjacent to this vertex 
-        for v in self.graph[u]: 
-            # If v is not visited yet, then make it a child of u 
-            # in DFS tree and recur for it 
-            if visited[v] == False : 
-                parent[v] = u 
-                self.bridgeUtil(v, visited, parent, low, disc) 
-  
-                # Check if the subtree rooted with v has a connection to 
-                # one of the ancestors of u 
-                low[u] = min(low[u], low[v]) 
-  
-  
-                ''' If the lowest vertex reachable from subtree 
-                under v is below u in DFS tree, then u-v is 
-                a bridge'''
-                if low[v] > disc[u]: 
-                    print ("%d %d" %(u,v)) 
-      
-                      
-            elif v != parent[u]: # Update low value of u for parent function calls. 
-                low[u] = min(low[u], disc[v]) 
-  
-        
-    # DFS based function to find all bridges. It uses recursive 
-    # function bridgeUtil() 
-    def bridge(self): 
-   
-        # Mark all the vertices as not visited and Initialize parent and visited,  
-        # and ap(articulation point) arrays 
-        visited = [False] * (self.V) 
-        disc = [float("Inf")] * (self.V) 
-        low = [float("Inf")] * (self.V) 
-        parent = [-1] * (self.V) 
-  
-        # Call the recursive helper function to find bridges 
-        # in DFS tree rooted with vertex 'i' 
-        for i in range(self.V): 
-            if visited[i] == False: 
-                self.bridgeUtil(i, visited, parent, low, disc) 
-          
-   
-# Create a graph given in the above diagram 
-g1 = Graph(5) 
-g1.addEdge(1, 0) 
-g1.addEdge(0, 2) 
-g1.addEdge(2, 1) 
-g1.addEdge(0, 3) 
-g1.addEdge(3, 4) 
+from collections import defaultdict
 
-  
-print("Bridges in first graph ", g1.graph)
-g1.bridge() 
-'''
-g2 = Graph(4) 
-g2.addEdge(0, 1) 
-g2.addEdge(1, 2) 
-g2.addEdge(2, 3) 
-print("\nBridges in second graph ")
-g2.bridge() 
-  
-   
+class Graph():
+    def __init__(self, vertices):
+        self.v = vertices
+        self.graph = defaultdict(list)
+        self.low = [float("inf")] * self.v
+        self.in_time = [float("inf")] * self.v
+        self.visited = [False] * self.v
+        self.timer = 0
+        
+    def addEdge(self, u, v):
+        self.graph[u].append(v)#we have to insert out graph in both way so that our algo work
+        self.graph[v].append(u)
+    def depthFirtstSearch(self, node, parent):
+        self.visited[node] = True
+        self.in_time[node] = self.timer
+        self.low[node] = self.timer
+        self.timer += 1
+        
+        for child in self.graph[node]:
+            if child == parent: #if the child of current node is its parents .. then do nothing
+                continue
+            if self.visited[child] == True:#edge node-child is a back edge
+                self.low[node] = min(self.low[node], self.in_time[child])#self.low[node] because when we come back after recursive call then we dont want ot change the low time of parent node from its child node
+            else:#edge node-child is forward edge
+                self.depthFirtstSearch(child, node)
+                if(self.low[child] > self.in_time[node]):#this line means if the  node is connected to any another node so its low time has reduced to the intime of that connected node's...This line will only be true if node is not connected to any other node other that its current parents
+                    print(f"node {node} - node {child} is bridge")
+                self.low[node] = min(self.low[node], self.low[child])#change the low of current node to its child beacuse if a child can reach a back track then parent can too 
 g3 = Graph (7) 
 g3.addEdge(0, 1) 
 g3.addEdge(1, 2) 
@@ -100,5 +37,17 @@ g3.addEdge(1, 4)
 g3.addEdge(1, 6) 
 g3.addEdge(3, 5) 
 g3.addEdge(4, 5) 
-print("\nBridges in third graph ")
-g3.bridge()'''
+print("\nBridges in this graph ")
+g3.depthFirtstSearch(0, -1)#we are initilizing out algo from 0 with no parent
+
+
+
+
+
+
+        
+    
+        
+
+
+        
